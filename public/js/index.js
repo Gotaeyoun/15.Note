2e1fcb4de96893428fd3dffe9173921396d04c25
Gotaeyoun <ohdin486@gmail.com>
2020-06-29 17:27:11 +0900

    누구나 연습장

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,9 @@
 
 
 
+var auth = firebase.auth();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+
 
 
 
@@ -14,13 +17,32 @@
 
 
 
-
 /************************이벤트 핸들러****************************/
-
-
+function onLogin(){
+	auth.signInWithPopup(googleAuth);
+}
+function onLogout(){
+	auth.signOut();
+}
+function onAuthStateChg(data){
+	console.log(data);
+	if(data){
+		$(".bt-login").hide();
+		$(".logout-wrap").addClass("d-flex").removeClass("d-none");
+		$(".header .icon").attr("src", data.photoURL);
+		$(".header .email").html(data.email);
+	}
+	else{
+		$(".bt-login").show();
+		$(".logout-wrap").addClass("d-none").removeClass("d-flex");
+	}
+}
 
 
 
 
 
 /************************이벤트 등록****************************/
+auth.onAuthStateChanged(onAuthStateChg);
+$(".bt-login").click(onLogin);
+$(".bt-logout").click(onLogout);
